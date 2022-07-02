f793813909d92fcf0780e74ff5f37eadfe887c7a
Andrey <>
2022-07-03 03:16:26 +0500

    two

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿string[] array1 = new string[5] {"1234", "1567", "-2", "world", "hello"};
 string[] array2 = new string[array1.Length];
 void Secondarray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Secondarray(array1, array2);
+PrintArray(array2);
