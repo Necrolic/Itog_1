67fff4f4e5761137921e25f33013a8151ee82dce
Andrey <>
2022-07-03 05:17:40 +0500

    ends

--- Programs.cs
+++ Programs.cs
@@ -1,4 +1,5 @@
-﻿string[] array1 = new string[5] {"1234", "1567", "-2", "world", "hello"};
+﻿
+string[] array1 = new string[5] {"1234", "1567", "-2", "world", "hello"};
 string[] array2 = new string[array1.Length];
 void Secondarray(string[] array1, string[] array2)
 {
